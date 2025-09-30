be943888b098b643072f9b8b9c21ee55280b50e1
Luis Gabriel Venegas Saucedo <venegassaucedoluis@gmail.com>
2025-09-30 14:27:32 -0600

    avance todo

--- to-do-app/main.js
+++ to-do-app/main.js
@@ -6,6 +6,10 @@ let btnCompletar = document.querySelector(".btn-completar");
 let btnEditar = document.querySelector(".btn-editar");
 let divTareas = document.querySelector(".tareas");
 let controles = document.querySelector(".controles");
+let tareasCompletas = document.querySelector(".tareasCompletas");
+let modoEdicion = false;
+let idTareaEditar = null;
+let tareas = [];
 
 
 let idTarea = 1;
@@ -15,7 +19,7 @@ btnAdd.addEventListener("click", function(){
     let tarea = inputTareas.value;
     if (tarea === ""){
         alert("Por favor introduce una tarea");
-    }else if (tarea.length > 1){
+    }else if (tarea.length > 0){
         listaTareas.innerHTML += `<div id="tarea-${idTarea}" class="bg-white p-4 rounded-lg shadow-md flex justify-between items-center my-2">
             <span>${tarea}</span>
             <input type="checkbox" class="mr-2">
@@ -23,28 +27,48 @@ btnAdd.addEventListener("click", function(){
         `;
         idTarea++;
         inputTareas.value = "";
+        tareas.push({
+            id: idTarea,
+            tarea: tarea,
+            completada: false
+        })
     }
 })
 
 btnEliminarTodas.addEventListener("click", function(){
     listaTareas.innerHTML = "";
     idTarea = 1;
+    tareas = [];
 })
 
 btnCompletar.addEventListener("click", function(){
     let tareasCompletadas = divTareas.querySelectorAll("input:checked");
     tareasCompletadas.forEach(function(tarea){
+        let tareaId = tarea.parentElement.id.split("-")[1];
+        tareaId = parseInt(tareaId);
+        tareas[tareaId].completada = true;
         tarea.parentElement.remove();
         idTarea--;
+        console.log(tareas);
+    })
+    tareas = tareas.filter(function(tarea){
+        return !tarea.completada;
     })
 
+    tareas.forEach(function(tarea){
+        if (tarea.completada){
+            tareasCompletas.innerHTML += `<div id="tarea-${tarea.id}" class="bg-white p-4 rounded-lg shadow-md flex justify-between items-center my-2">
+            <span>${tarea.tarea}</span>
+            <input type="checkbox" class="mr-2" checked>
+        </div>
+        `;
+        }
+    })
     
 })
 
-btnEditar.addEventListener("click", function(){
-    let tareaAeditar = divTareas.querySelectorAll("input:checked");
-    inputTareas.value = tareaAeditar[0].parentElement.querySelector("span").textContent;
-    btnEditar.innerHTML = 'Aceptar'
 
+
+btnEditar.addEventListener("click", function(){
 
 })
