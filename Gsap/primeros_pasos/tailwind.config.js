ff0f7af72802733b9de5d935290dc670bcd9a9c6
Luis Gabriel Venegas Saucedo <venegassaucedoluis@gmail.com>
2025-10-07 19:06:58 -0600

    cambios Gsap

---
+++ Gsap/primeros_pasos/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins"', 'sans-serif'],
+      },
+      fontWeight: {
+        'thin': 100,
+        'extralight': 200,
+        'light': 300,
+        'regular': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 800,
+        'black': 900,
+      },
+      colors: {
+        'fondo': '#002b2e',
+      }
+    },
+  },
+  plugins: [],
+}
